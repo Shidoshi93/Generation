7317b336b12b222ec728dbadebb98a47e2d06e24
Daniel Ribeiro <dann.ribeiroo@gmail.com>
2025-10-31 08:48:51 -0300

    feat: rename variables

--- javascript/aula5/CalculaProduto.js
+++ javascript/aula5/CalculaProduto.js
@@ -1,19 +1,19 @@
 import readlineSync from 'readline-sync';
 
-let num1 = 0;
-let num2 = 0;
-let num3 = 0;
-let num4 = 0;
-let result = 0;
+let numero1 = 0;
+let numero2 = 0;
+let numero3 = 0;
+let numero4 = 0;
+let resultado = 0;
 
-num1 = readlineSync.questionFloat("Digite o primeiro número: ");
-num2 = readlineSync.questionFloat("Digite o segundo número: ");
-num3 = readlineSync.questionFloat("Digite o terceiro número: ");
-num4 = readlineSync.questionFloat("Digite o quarto número: ");
+numero1 = readlineSync.questionFloat("Digite o primeiro número: ");
+numero2 = readlineSync.questionFloat("Digite o segundo número: ");
+numero3 = readlineSync.questionFloat("Digite o terceiro número: ");
+numero4 = readlineSync.questionFloat("Digite o quarto número: ");
 
-result = (num1 * num2) - (num3 * num4);
+resultado = (numero1 * numero2) - (numero3 * numero4);
 
 console.log(
     `O resultado do cálculo de produto é: 
-    ${result.toFixed(2)}`
+    ${resultado.toFixed(2)}`
 );
